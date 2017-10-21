d7aa9e03dfb31c63a8244044e7bd264b11aa93a9
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-21 12:44:07 -0700

    created loginview model and view

--- lab27_brian/lab27_brian/Models/RegistrationViewModel.cs
+++ lab27_brian/lab27_brian/Models/RegistrationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Cache;
@@ -9,6 +10,12 @@ namespace lab27_brian.Models
 {
     public class RegistrationViewModel
     {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
         [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
